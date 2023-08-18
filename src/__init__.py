3d09dc06365f364348aa2072bb23282546949372
Leopold Michael <leomi12042004@gmail.com>
2023-08-19 01:55:14 +0200

    Slight adjustment of the sprites and placement

--- src/__init__.py
+++ src/__init__.py
@@ -28,7 +28,7 @@ DT = 1 / FPS
 BLIT_OFFSET = np.array((0, -26))
 
 AXLE_POS = np.array((957, 536))
-SECOND_POS = np.array((956, 716))
+SECOND_POS = np.array((956, 717))
 DATE_POS = np.array((1287 + 10, 504 + 7))
 
 BG.blit(spr.CASING, BLIT_OFFSET)
